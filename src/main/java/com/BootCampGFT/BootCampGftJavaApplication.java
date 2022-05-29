a4ba0546135512909cc3abdd8e4412e91d9d1bfc
Marcilio Claudino Araujo <marcilioclaudinoaraujo@gmail.com>
2022-05-29 09:22:15 -0300

    Primeira Versao

---
+++ src/main/java/com/BootCampGFT/BootCampGftJavaApplication.java
@@ -0,0 +1,13 @@
+package com.BootCampGFT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCampGftJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCampGftJavaApplication.class, args);
+	}
+
+}
